f453c042bb726b311a4dae2693d2ac921cefb375
Henrique de Freitas Almeida <56054329+detowhey@users.noreply.github.com>
2023-09-18 16:40:35 -0300

    add fakerJS in the project

--- tests/app-web-bank.spec.ts
+++ tests/app-web-bank.spec.ts
@@ -1,23 +1,26 @@
 import { test } from '@playwright/test';
+import { fakerPT_BR as faker } from '@faker-js/faker';
 
-test.describe('Simple page tests clicks"', () => {
+test.describe('Simple page tests clicks', () => {
 
     test.beforeEach(async ({ page }) => {
-        await page.goto('https://www.way2automation.com/angularjs-protractor/banking/#/login');
+        await page.goto('');
     });
 
     test('Click all buttons in the the aplication', async ({ page }) => {
+        const fakerData = faker;
+
         await page.getByRole('button', { name: 'Customer Login' }).click();
         await page.getByRole('button', { name: 'Home' }).click();
         await page.getByText('Home XYZ Bank Logout Customer Login Bank Manager Login').click();
         await page.getByRole('button', { name: 'Bank Manager Login' }).click();
         await page.getByRole('button', { name: 'Add Customer' }).click();
         await page.getByPlaceholder('First Name').click();
-        await page.getByPlaceholder('First Name').fill('name');
+        await page.getByPlaceholder('First Name').fill(fakerData.person.firstName());
         await page.getByPlaceholder('First Name').press('Tab');
-        await page.getByPlaceholder('Last Name').fill('last name');
+        await page.getByPlaceholder('Last Name').fill(fakerData.person.lastName());
         await page.getByPlaceholder('Last Name').press('Tab');
-        await page.getByPlaceholder('Post Code').fill('92440580');
+        await page.getByPlaceholder('Post Code').fill(fakerData.location.zipCode());
         await page.getByRole('form').getByRole('button', { name: 'Add Customer' }).click();
     });
 });
