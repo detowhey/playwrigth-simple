aa20ccf86e7c33aca8afaf7702119f042719499a
Henrique Almeida SoftDesign <henrique.almeida@3rd.braza.com.br>
2023-10-20 09:54:56 -0300

    create enum file

---
+++ src/enum/envs.enum.ts
@@ -0,0 +1,13 @@
+enum Envs {
+    report = 'QASE_REPORT',
+    apiToken = 'QASE_API_TOKEN',
+    basePath = 'QASE_API_BASE_URL',
+    rootSuiteTitle = 'QASE_ROOT_SUITE_TITLE',
+    projectCode = 'QASE_PROJECT_CODE',
+    runId = 'QASE_RUN_ID',
+    runName = 'QASE_RUN_NAME',
+    runDescription = 'QASE_RUN_DESCRIPTION',
+    runComplete = 'QASE_RUN_COMPLETE',
+    environmentId = 'QASE_ENVIRONMENT_ID',
+    uploadAttachments = 'QASE_UPLOAD_ATTACHMENTS',
+}
